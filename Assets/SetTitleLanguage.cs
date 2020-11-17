2032b2fc3b9787c46a311af67096c3a7e9db997a
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2020-11-17 12:24:58 +0200

    Main titles change to language selected

---
+++ Assets/SetTitleLanguage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SetTitleLanguage : MonoBehaviour
+{
+    private const string LANGUAGE = "Language";
+    private const int ENGLISH = 1;
+    private const int AFRIKAANS = 2;
+    [SerializeField] TextMeshProUGUI flashText;
+    [SerializeField] TextMeshProUGUI socialStoryText;
+    [SerializeField] TextMeshProUGUI mcqText;
+    [SerializeField] TextMeshProUGUI puzzleText;
+
+    private void Start()
+    {
+        UpdateLanguage();
+    }
+
+    public void SetEnglish()
+    {
+        flashText.text = "Flashcards";
+        socialStoryText.text = "Social Stories";
+        mcqText.text = "Multiple Choice Tests";
+        puzzleText.text = "Puzzles";
+    }
+
+    public void SetAfrikaans()
+    {
+        flashText.text = "Flitskaarte";
+        socialStoryText.text = "Sociale Stories";
+        mcqText.text = "Meervoudige Keusetoetse";
+        puzzleText.text = "Ruigels";
+    }
+
+    public void UpdateLanguage()
+    {
+        if(PlayerPrefs.GetInt(LANGUAGE) == ENGLISH)
+        {
+            SetEnglish();
+        }
+
+        if (PlayerPrefs.GetInt(LANGUAGE) == AFRIKAANS)
+        {
+            SetAfrikaans();
+        }
+    }
+}
