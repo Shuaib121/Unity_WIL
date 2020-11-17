13e9510a8a4d0ec9e4864b340ec2603639537f3e
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2020-11-17 11:18:19 +0200

    Added popup language selection

---
+++ Assets/LanguageSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSelection : MonoBehaviour
+{
+    private const string LANGUAGE = "Language";
+    private const int ENGLISH = 1;
+    private const int AFRIKAANS = 2;
+    [SerializeField] GameObject languagePopup;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey(LANGUAGE))
+        {
+            languagePopup.SetActive(true);
+        }
+    }
+    public void SetEnglish()
+    {
+        PlayerPrefs.SetInt(LANGUAGE, ENGLISH);
+    }
+
+    public void SetArikaans()
+    {
+        PlayerPrefs.SetInt(LANGUAGE, AFRIKAANS);
+    }
+}
