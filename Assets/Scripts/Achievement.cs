c8c2c2df03a76c5df1bf3f937e6d17af4c59e0b5
Byron <byronrvarty44@gmail.com>
2020-11-26 09:41:48 +0200

    update

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement
+{
+    public string Title;
+    public string Description;
+    public int difficulty;
+}
