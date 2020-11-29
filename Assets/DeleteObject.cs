273efaaafb41d663d48f4a185f2e10d39297be31
Byron <byronrvarty44@gmail.com>
2020-11-29 18:45:43 +0200

    structure for inserting almost done

---
+++ Assets/DeleteObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    public void Delete()
+    {
+        Destroy(gameObject.gameObject);
+    }
+}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,25 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using static NativeGallery;
+using TMPro;
 
 public class TestScript : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI tmp;
-	[SerializeField] Image image;
+	//[SerializeField] TextMeshProUGUI tmp;
+	//[SerializeField] Image image;
+	public GameObject cell;
+
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+        for (int i = 0; i < 5; i++)
+		{
+			GameObject item = Instantiate(cell).gameObject;
+			GameObject content = GameObject.Find("Content");
+			item.transform.SetParent(content.transform);
+			item.transform.Find("Image").GetComponent<Image>().color = Color.black;
+		}
+
+	}
 
 	public void PickImage(int maxSize)
 	{
 
-		NativeGallery.Permission permission = NativeGallery.GetImagesFromGallery((path) =>
+		Permission permission = GetImagesFromGallery((path) =>
 		{
+            for (int i = 0; i < path.Length; i++)
+            {
+				Texture2D texture = LoadImageAtPath(path[i], maxSize, false);
+				GameObject item = Instantiate(cell).gameObject;
+				GameObject content = GameObject.Find("Content");
+				item.transform.SetParent(content.transform);
+				item.transform.Find("Image").GetComponent<Image>().sprite =
+					Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0f, 0f));
+			}
+
+
+			/*
 			Debug.Log("Image path: " + path);
 			tmp.text = path[0];
 			if (path != null)
@@ -33,10 +52,22 @@ public class TestScript : MonoBehaviour
 				}
 
 				image.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-			}
+
+			}*/
 		}, "Select an image", "image/*");
 
 		Debug.Log("Permission result: " + permission);
+	}
+
+	public void Finish()
+    {
+		GameObject content = GameObject.Find("Content");
+
+        foreach (Transform child in content.transform)
+        {
+			string tts = child.Find("Image").GetChild(0).GetChild(0).Find("Text").GetComponent<TextMeshProUGUI>().text;
+			Debug.Log(tts);
+        }
 	}
 
 }
