8c62021d7666a265f6bc298ca9640b2563be860c
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2020-12-01 10:53:48 +0200

    Update TestScript.cs

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,121 +1,107 @@
-﻿using SQLite4Unity3d;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using static NativeGallery;
+using TMPro;
+using System.Collections.Generic;
+using SQLite4Unity3d;
+using System.Linq;
 
 public class TestScript : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI tmp; //tmp for testing purposes
-    [SerializeField] Image image;
-    [SerializeField] TMP_Dropdown dropdownMenu;
+	//[SerializeField] TextMeshProUGUI tmp;
+	//[SerializeField] Image image;
+	public GameObject cell;
+	List<string> categories = new List<string>();
 
-    private readonly DataService ds = new DataService("MainDatabase.db");
-    private List<FlashcardsTable> flashcards = new List<FlashcardsTable>();
-    private SQLiteConnection db;
-    private List<ImageText> images = new List<ImageText>();
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-        db = ds.GetConnection();
-        Initialize();
+		DataService ds = new DataService("MainDatabase.db");
+		List<FlashcardsTitleTable> d = ds.GetFlashcardTitles().ToList();
+		categories.Clear();
 
-        dropdownMenu.onValueChanged.AddListener(delegate
-        {
-            tmp.text = dropdownMenu.options[dropdownMenu.value].text;
-        });
+		foreach (var item in d)
+		{
+			if (item.Language == PlayerPrefs.GetInt("Language"))
+			{
+				categories.Add(item.FlashcardTitle);
+			}
+		}
 
-    }
+		for (int i = 0; i < 5; i++)
+		{
+			GameObject item = Instantiate(cell).gameObject;
+			GameObject content = GameObject.Find("Content");
+			item.transform.SetParent(content.transform);
+			item.transform.Find("Image").GetComponent<Image>().color = Color.black;
 
-    //A class just to store the image and its text
-    private class ImageText
-    {
-        public Texture2D image;
-        public string imageText;
+			item.transform.Find("Image").Find("Dropdown").GetComponent<TMP_Dropdown>().ClearOptions();
+			item.transform.Find("Image").Find("Dropdown").GetComponent<TMP_Dropdown>().AddOptions(categories);
+		}
+	}
 
-        public ImageText(Texture2D image, string imageText) 
-        {
-            this.image = image;
-            this.imageText = imageText;
-        }
-    }
-
-    //Populates the dropdown menu with flashcard titles so it can be used as the category when inserting
-    public void Initialize()
-    {
-        dropdownMenu.ClearOptions();
-        var tempList = ds.GetFlashcardTitles().ToList();
-        List<string> options = new List<string>();
-        foreach (var card in tempList)
-        {
-            options.Add(card.FlashcardTitle);
-        }
-
-        dropdownMenu.AddOptions(options);
-    }
-
-    //Asks the user to pick an image form their gallery and saves it in a string(path)
-    public void PickImage(int maxSize) 
-    {
-        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
-        {
-            Debug.Log("Image path: " + path);
-            if (path != null)
+	public void PickImage(int maxSize)
+	{
+		Permission permission = GetImagesFromGallery((path) =>
+		{
+            for (int i = 0; i < path.Length; i++)
             {
-                // Create Texture from selected image
-                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize, false);
-                if (texture == null)
-                {
-                    Debug.Log("Couldn't load texture from " + path);
-                    return;
-                }
-                images.Add(new ImageText(texture,""));
-                image.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-            }
-        }, "Select an image", "image/*");
-        Debug.Log("Permission result: " + permission);
-    }
+				Texture2D texture = LoadImageAtPath(path[i], maxSize, false);
+				GameObject item = Instantiate(cell).gameObject;
+				GameObject content = GameObject.Find("Content");
+				item.transform.SetParent(content.transform);
+				item.transform.Find("Image").GetComponent<Image>().sprite =
+					Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
 
-    //turns an image into a flashcard, currently does not get the text for it but that can be done with a parameter
-    private void UpdateImageList()
+				item.transform.Find("Image").Find("Dropdown").GetComponent<TMP_Dropdown>().ClearOptions();
+				item.transform.Find("Image").Find("Dropdown").GetComponent<TMP_Dropdown>().AddOptions(categories);
+			}
+
+			/*
+			Debug.Log("Image path: " + path);
+			tmp.text = path[0];
+			if (path != null)
+			{
+				// Create Texture from selected image
+				Texture2D texture = NativeGallery.LoadImageAtPath(path[0], maxSize);
+				if (texture == null)
+				{
+					Debug.Log("Couldn't load texture from " + path);
+					return;
+				}
+
+				image.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+			}*/
+
+		}, "Select an image", "image/*");
+
+		Debug.Log("Permission result: " + permission);
+	}
+
+	public void Finish()
     {
-        flashcards.Clear();
-        int counter = 0;
-        tmp.text = "POPULATING";
-        foreach (var image in images)
+		GameObject content = GameObject.Find("Content");
+		var dbPath = string.Format(@"Assets/StreamingAssets/{0}", "MainDatabase.db");
+		var db = new SQLiteConnection(dbPath);
+		db.CreateTable<FlashcardsTable>();
+
+		foreach (Transform child in content.transform)
         {
-            byte[] imageData = image.image.EncodeToPNG();
-            var currentCategory = dropdownMenu.options[dropdownMenu.value].text;
-            tmp.text = "select count(*) from FlashcardsTable where Category = '" + currentCategory + "'";
+			string english = child.Find("Image").Find("English").GetChild(0).Find("Text").GetComponent<TextMeshProUGUI>().text;
+			TMP_Dropdown dropdown = child.Find("Image").Find("Dropdown").GetComponent<TMP_Dropdown>();
+			string category = dropdown.options[dropdown.value].text;
 
-            var categoryCount = db.ExecuteScalar<int>("select count(*) from FlashcardsTable where Category = '" + currentCategory + "'");
-            FlashcardsTable card = new FlashcardsTable();
-            card.FlashcardName = currentCategory + categoryCount++;
-            card.FlashcardText = image.imageText;
-            card.Category = currentCategory;
-            card.ImageData = imageData;
+			byte[] imageData = child.Find("Image").GetComponent<Image>().sprite.texture.EncodeToPNG();
 
-            counter++;
-            flashcards.Add(card);
-        }
-
-    }
-
-    //Inserts cards into the database
-    public void InsertCardsToDB()
-    {
-        tmp.text = "Insterting";
-        int counter = 0;
-        foreach (var card in flashcards)
-        {
-            db.Insert(card);
-            counter++;
-        }
-        tmp.text = "Insterted: " + counter;
-    }
-
-
+			FlashcardsTable s = new FlashcardsTable()
+			{
+				Category = category,
+				FlashcardName = english,
+				FlashcardText = english,
+				ImageData = imageData
+			};
+			db.Insert(s);
+		}
+	}
 }
