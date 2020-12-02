b2fcd0c6174f4e2949492658077745cc523578a1
Byron <byronrvarty44@gmail.com>
2020-12-02 14:50:17 +0200

    Added translation for edit selection

---
+++ Assets/MenuItemTranslator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+public class MenuItemTranslator : MonoBehaviour
+{
+    [SerializeField] string English;
+    [SerializeField] string Afrikaans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Language") == 1)
+        {
+            GetComponent<TextMeshProUGUI>().text = English;
+        }else
+        {
+            GetComponent<TextMeshProUGUI>().text = Afrikaans;
+        }
+    }
+}
