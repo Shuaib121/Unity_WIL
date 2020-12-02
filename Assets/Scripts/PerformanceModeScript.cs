0f69e875a16c61d2dc8312b8e9d7ad9ff6e1f196
Byron <byronrvarty44@gmail.com>
2020-12-02 13:00:06 +0200

    Updated main menu dialog - low performance and edit mode done

---
+++ Assets/Scripts/PerformanceModeScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PerformanceModeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("LowPerformance") == 1)
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 30;
+        }
+    }
+
+    public void UpdatePerformanceMode()
+    {
+        if (GameObject.Find("TogglePerformance").GetComponent<Toggle>().isOn)
+        {
+            PlayerPrefs.SetInt("LowPerformance", 1);
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 30;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("LowPerformance", 0);
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 90;
+        }
+    }
+}
