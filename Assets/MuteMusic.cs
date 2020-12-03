a28156ba5420bb9add86e77cc96c481ee92cf384
Shuaib Ebrahim <ebrahimshuaib1@gmail.com>
2020-12-03 14:08:22 +0200

    Added audio to scenes

---
+++ Assets/MuteMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteMusic : MonoBehaviour
+{
+    private int musicCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicCheck = PlayerPrefs.GetInt("MuteMusic");
+
+        if (musicCheck == 1)
+        {
+            GetComponent<AudioSource>().enabled = false;
+        }
+    }
+
+}
